adaf34395f53dc7ab2ca16235e92d1b565915dfb
Robert Love <rlove@peakbiz.com>
2014-01-08 22:54:59 -0700

    Added Support building and running tests on Mac OS.
    -DUnit.AutoDetect.Console Unit Added
       Using this unit will auto detect the platform you are compiling for and register the correct console unit.
    -DUnitX.Detour.pas Removed
      Not needed with the changes to DUnitX.Utils
      Required the windows platform, and there is a different way to accomplish what this did in a cross platform way.
    -DUnit.MacOS.Console Modified
      Changed to support basic ANSI colors, fixed compiler define, and documented unit
    -DUnitX.Utils.pas
      Added DSharp License to indicate where portions of  code was taken from
      Removed Dependancy on Detours.
    -DUnitX.Expert.CodeGen.Template
      Changed the new project to use DUnitX.AutoDetect.Console instead of DUnitX.Windows.Console.

--- DUnitX.Utils.pas
+++ DUnitX.Utils.pas
@@ -23,6 +23,40 @@
 {  limitations under the License.                                           }
 {                                                                           }
 {***************************************************************************}
+{                                                                           }
+{ Portions of the file also fall under the following license                }
+{ as they were taken from the DSharp Project                                }
+{ https://bitbucket.org/sglienke/dsharp                                     }
+{                                                                           }
+(*
+  Copyright (c) 2011-2012, Stefan Glienke
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+  - Redistributions of source code must retain the above copyright notice,
+    this list of conditions and the following disclaimer.
+  - Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+  - Neither the name of this library nor the names of its contributors may be
+    used to endorse or promote products derived from this software without
+    specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+  POSSIBILITY OF SUCH DAMAGE.
+*)
+
 
 unit DUnitX.Utils;
 
@@ -610,7 +644,9 @@ type
     FPropInfo: TPropInfo;
     FGetter: TFunc<Pointer, TValue>;
     FSetter: TProc<Pointer, TValue>;
-    class var FRegister: TDictionary<TPair<PTypeInfo, string>, TRttiPropertyExtension>;
+    class var
+      FRegister: TDictionary<TPair<PTypeInfo, string>, TRttiPropertyExtension>;
+      FPatchedClasses: TDictionary<TClass, TClass>;
     function GetIsReadableStub: Boolean; //override;
     function GetIsWritableStub: Boolean; //override;
     function DoGetValueStub(Instance: Pointer): TValue; //override;
@@ -637,6 +673,7 @@ type
     property Getter: TFunc<Pointer, TValue> read FGetter write FGetter;
     property Setter: TProc<Pointer, TValue> read FSetter write FSetter;
   end;
+
 
   TArrayHelper = class
   public
@@ -691,8 +728,7 @@ uses
   Generics.Defaults,
   Math,
   StrUtils,
-  SysConst,
-  DUnitX.Detour;
+  SysConst;
 
 var
   Context: TRttiContext;
@@ -2860,13 +2896,24 @@ end;
 
 class constructor TRttiPropertyExtension.Create;
 begin
-  TRttiPropertyExtension.InitVirtualMethodTable;
-
   FRegister := TObjectDictionary<TPair<PTypeInfo, string>, TRttiPropertyExtension>.Create([doOwnsValues]);
+  FPatchedClasses := TDictionary<TClass, TClass>.Create;
+
+  TRttiPropertyExtension.InitVirtualMethodTable;
 end;
 
 class destructor TRttiPropertyExtension.Destroy;
+var
+  LClass: TClass;
+  LPointer: Pointer;
 begin
+  for LClass in FPatchedClasses.Values do
+  begin
+    LPointer := PByte(LClass) + vmtSelfPtr;
+    FreeMem(LPointer);
+  end;
+
+  FPatchedClasses.Free;
   FRegister.Free;
 end;
 
@@ -2878,6 +2925,8 @@ begin
   Init(GetRttiType(Parent), @FPropInfo);
 
   FRegister.Add(TPair<PTypeInfo, string>.Create(Parent, Name), Self);
+
+  PPointer(Self)^ := FPatchedClasses[Self.ClassType];
 end;
 
 function TRttiPropertyExtension.DoGetValue(Instance: Pointer): TValue;
@@ -2975,12 +3024,28 @@ begin
 end;
 
 class procedure TRttiPropertyExtension.InitVirtualMethodTable;
+const
+  MaxIndex = 17;  // TRttiInstanceProperty.GetPropInfo
+{$POINTERMATH ON}
+type
+  PVtable = ^Pointer;
+{$POINTERMATH OFF}
+var
+  LSize: Integer;
+  LData: Pointer;
+  LPatchedClass: TClass;
 begin
-  OverrideVirtualMethod(Self, 5, @TRttiPropertyExtension.GetIsReadableStub);
-  OverrideVirtualMethod(Self, 6, @TRttiPropertyExtension.GetIsWritableStub);
-  OverrideVirtualMethod(Self, 7, @TRttiPropertyExtension.DoGetValueStub);
-  OverrideVirtualMethod(Self, 8, @TRttiPropertyExtension.DoSetValueStub);
-  OverrideVirtualMethod(Self, 12, @TRttiPropertyExtension.GetPropInfoStub);
+  LSize := SizeOf(Pointer) * (1 + MaxIndex - (vmtSelfPtr div SizeOf(Pointer)));
+  LData := AllocMem(LSize);
+  LPatchedClass := TClass(PByte(LData) - vmtSelfPtr);
+  FPatchedClasses.Add(Self, LPatchedClass);
+  Move((PByte(Self) + vmtSelfPtr)^, LData^, LSize);
+
+  PVtable(LPatchedClass)[5] := @TRttiPropertyExtension.GetIsReadableStub;
+  PVtable(LPatchedClass)[6] := @TRttiPropertyExtension.GetIsWritableStub;
+  PVtable(LPatchedClass)[7] := @TRttiPropertyExtension.DoGetValueStub;
+  PVtable(LPatchedClass)[8] := @TRttiPropertyExtension.DoSetValueStub;
+  PVtable(LPatchedClass)[12] := @TRttiPropertyExtension.GetPropInfoStub;
 end;
 
 
